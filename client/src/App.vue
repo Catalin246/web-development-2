e6b98dc648ae3551d14caa589861d6775081b925
Catalin246 <avorniceseicatalin1234@gmail.com>
2025-05-06 16:41:19 +0200

    Add shadow to some elements.

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,12 +9,12 @@ const route = useRoute()
   <div class="w-screen h-screen overflow-hidden flex flex-col bg-white">
 
     <!-- Fixed Header -->
-    <header class="fixed top-0 left-0 w-full z-10 bg-blue-600 text-white p-4 flex justify-between items-center shadow-none">
+    <header class="fixed top-0 left-0 w-full z-10 bg-blue-600 text-white p-5 flex justify-between items-center shadow-xl rounded-b-2xl md:rounded-none">
       <h1 class="font-medium text-xl">Chat App</h1>
     </header>
 
     <!-- Main Layout with Scroll -->
-    <div class="flex flex-1 h-full mt-[64px] mb-[56px] md:mb-0 overflow-y-auto"> <!-- adjust mt and mb to match header/footer height -->
+    <div class="flex flex-1 h-full py-16 md:py-0 md:pt-16 md:mb-0 overflow-y-auto"> <!-- adjust mt and mb to match header/footer height -->
 
       <!-- Sidebar (10%) -->
       <aside class="hidden md:flex flex-col w-[6%] bg-gray-100 py-4 items-center space-y-6">
@@ -31,7 +31,7 @@ const route = useRoute()
     </div>
 
     <!-- Fixed Mobile Bottom Nav -->
-    <footer class="block md:hidden fixed bottom-0 left-0 w-full bg-white flex justify-around py-2 border-t shadow-md z-10">
+    <footer class="block md:hidden fixed bottom-0 left-0 w-full bg-white flex justify-around py-2 z-10 rounded-t-2xl shadow-top">
       <NavIcon label="Chats" :active="route.path === '/chats'" />
       <NavIcon label="Groups" :active="route.path === '/groups'" />
       <NavIcon label="Profile" :active="route.path === '/profile'" />
